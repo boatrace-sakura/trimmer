2b4346613f711c3fd012462293f1fba5d7568ae2
shimomo <developer@shimomo.net>
2024-10-27 15:49:03 +0900

    Rename mask to characters

--- src/MainTrimmer.php
+++ src/MainTrimmer.php
@@ -11,31 +11,31 @@ class MainTrimmer
 {
     /**
      * @param  string|null  $value
-     * @param  string       $mask
+     * @param  string       $characters
      * @return string|null
      */
-    public function trim(?string $value, string $mask = "\040\t\n\r\0\x0B"): ?string
+    public function trim(?string $value, string $characters = "\040\t\n\r\0\x0B"): ?string
     {
-        return is_null($value) ? null : trim($value, $mask);
+        return is_null($value) ? null : trim($value, $characters);
     }
 
     /**
      * @param  string|null  $value
-     * @param  string       $mask
+     * @param  string       $characters
      * @return string|null
      */
-    public function ltrim(?string $value, string $mask = "\040\t\n\r\0\x0B"): ?string
+    public function ltrim(?string $value, string $characters = "\040\t\n\r\0\x0B"): ?string
     {
-        return is_null($value) ? null : ltrim($value, $mask);
+        return is_null($value) ? null : ltrim($value, $characters);
     }
 
     /**
      * @param  string|null  $value
-     * @param  string       $mask
+     * @param  string       $characters
      * @return string|null
      */
-    public function rtrim(?string $value, string $mask = "\040\t\n\r\0\x0B"): ?string
+    public function rtrim(?string $value, string $characters = "\040\t\n\r\0\x0B"): ?string
     {
-        return is_null($value) ? null : rtrim($value, $mask);
+        return is_null($value) ? null : rtrim($value, $characters);
     }
 }
